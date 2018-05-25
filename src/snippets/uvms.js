fbd2c250746be3571551511221d8aed155ce8534
bygage <gage@ub.com>
2018-05-25 11:01:45 +0000

    first commit

---
+++ src/snippets/uvms.js
@@ -0,0 +1,14 @@
+define("ace/snippets/uvms",["require","exports","module"], function(require, exports, module) {
+    "use strict";
+
+    exports.snippetText =undefined;
+    exports.scope = "uvms";
+});
+                (function() {
+                    window.require(["ace/snippets/uvms"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            

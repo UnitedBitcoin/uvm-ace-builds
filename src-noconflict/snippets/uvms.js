6df8356386617053ee09b4fe8f053a4f6e2e4f0d
bygage <gage@ub.com>
2018-05-25 11:02:14 +0000

    all buildds

---
+++ src-noconflict/snippets/uvms.js
@@ -0,0 +1,14 @@
+ace.define("ace/snippets/uvms",["require","exports","module"], function(require, exports, module) {
+    "use strict";
+
+    exports.snippetText =undefined;
+    exports.scope = "uvms";
+});
+                (function() {
+                    ace.require(["ace/snippets/uvms"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
